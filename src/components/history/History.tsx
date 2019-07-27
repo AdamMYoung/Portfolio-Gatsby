d85fb6c98b3ef531244657d480c4e5b4757ac07c
Adam Young <adamyoungg97@gmail.com>
2019-07-27 21:44:26 +0100

    Updated react-scripts and added latest job history

--- src/components/history/History.tsx
+++ src/components/history/History.tsx
@@ -4,6 +4,7 @@ import InfoCard from "../elements/card/InfoCard";
 import office from "../../media/office.jpg";
 import teesside from "../../media/teesside-uni.jpg";
 import laptop from "../../media/code-laptop.png";
+import terraquest from "../../media/terraquest-logo.jpg"
 import Timeline from "./timeline/Timeline";
 import TimelineEntry, { Sides } from "./timeline/TimelineEntry";
 
@@ -33,7 +34,8 @@ export default class History extends Component {
                   title="DuPont Teijin Films"
                   image={office}
                   subtitle="2017-2018"
-                  description="I started a placement year at DuPont Teijin Films, where I worked on a variety of C# plotting and management applications."
+                  description="My first development role was a placement year at DuPont Teijin Films, where I worked on a variety of internal C# plotting and management
+                   applications, managed and maintained database, and provided support across the business."
                 />
               </TimelineEntry>
               <TimelineEntry>
@@ -44,6 +46,14 @@ export default class History extends Component {
                   description="I started final year where I developed Vocalia, a web-based podcast consumption and creation platform built with .NET Core and React.
                    This won the British Computer Society award during ExpoTees, the university's project exhibition day."
                 />
+              </TimelineEntry>
+              <TimelineEntry side={Sides.Right}>
+                <InfoCard
+                title="Terraquest Solutions"
+                image={terraquest}
+                subtitle="2019-Present"
+                description="My first graduate role is at Terraquest Solutions, where I work primarily as a front-end 
+                React developer on a variety of internal and external applications."/>
               </TimelineEntry>
             </Timeline>
           </Col>
