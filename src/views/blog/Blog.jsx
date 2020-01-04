4b63a8373b4d9078ba77e9ff33a23a994f2edb98
Adam Young <adamyoungg97@gmail.com>
2020-01-04 20:41:09 +0000

    Fixes to blog padding

--- src/views/blog/Blog.jsx
+++ src/views/blog/Blog.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Blog = () => (
-  <div style={{ marginTop: "40vh", marginLeft: 24 }}>
+  <div style={{ marginTop: "40vh", height: "90vh", marginLeft: 24 }}>
     <h1>Coming Soon...</h1>
   </div>
 );
