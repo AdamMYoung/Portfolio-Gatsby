1d844bda76628f05d83ade32c6632f176a3e76cf
Adam Young <adamyoungg97@gmail.com>
2020-01-24 13:05:29 +0000

    Added GA and web.config for azure deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
+import { createBrowserHistory } from "history";
+import ReactGA from "react-ga";
+
 import Layout from "./Layout";
+
+ReactGA.initialize("UA-140587584-1");
+
+const history = createBrowserHistory();
+history.listen(route => ReactGA.pageview(route.pathname));
 
 const App = () => {
   return (
-    <Router>
+    <Router history={history}>
       <Layout />
     </Router>
   );
