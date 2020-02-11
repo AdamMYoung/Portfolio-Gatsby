a8f018049d838f4061f13e2bc0d48bb6a6dbcd62
Adam Young <adamyoungg97@gmail.com>
2020-02-11 22:27:06 +0000

    Removed animated background and custom scrollbar

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,8 +1,4 @@
 import React, { useState, useRef, useEffect, Suspense } from "react";
-import SimpleBar from "simplebar-react";
-import * as p5 from "p5";
-
-import TOPOLOGY from "./animation/vanta.topology.js";
 
 import Navbar from "./navigation/Navbar";
 import Routes from "./routes/Routes";
@@ -10,45 +6,20 @@ import Footer from "./navigation/Footer";
 import Placeholder from "./views/Placeholder";
 
 const Layout = () => {
-  const [color, setColor] = useState("#2635a6");
-  const [vantaEffect, setVantaEffect] = useState(null);
-
-  const bgRef = useRef(null);
-  const scrollRef = useRef(null);
-
-  //Updates the background when the page loads.
-  useEffect(() => {
-    if (!vantaEffect) {
-      setVantaEffect(
-        TOPOLOGY({
-          el: bgRef.current,
-          p5: p5,
-          color: parseInt("00b0ff", 16),
-          backgroundColor: parseInt("00b0ff", 16)
-        })
-      );
-    }
-
-    return () => {
-      if (vantaEffect) vantaEffect.destroy();
-    };
-  }, [vantaEffect]);
+  const [color, setColor] = useState("#002f6c");
 
   return (
-    <div
-      ref={bgRef}
-      style={{ height: "100vh", width: "100vw", backgroundColor: color, transition: "background-color 1.5s ease" }}
-    >
-      <SimpleBar scrollableNodeProps={{ ref: scrollRef }} style={{ height: "100vh" }}>
-        <div style={{ position: "absolute", right: 0, zIndex: 1000 }}>
+    <div style={{ backgroundColor: color, transition: "background-color 1.5s ease" }}>
+      <div style={{ overflow: "auto", height: "100vh" }}>
+        <div style={{ position: "absolute", right: 15, zIndex: 1000 }}>
           <Navbar color={color} onColourChanged={color => setColor(color)} />
         </div>
         <Suspense fallback={<Placeholder />}>
           <Routes color={color} />
         </Suspense>
 
-        <Footer onScroll={() => (scrollRef.current.scrollTop = 0)} />
-      </SimpleBar>
+        <Footer onScroll={() => window.scrollTo(0)} />
+      </div>
     </div>
   );
 };
