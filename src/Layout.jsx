7b87072b59ff147798d864d4b738adf43404188a
Adam Young <adamyoungg97@gmail.com>
2020-02-12 23:26:07 +0000

    Improved background

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,4 +1,6 @@
-import React, { useState, useRef, useEffect, Suspense } from "react";
+import React, { useState, Suspense } from "react";
+
+import background from "./static/5-dots.png";
 
 import Navbar from "./navigation/Navbar";
 import Routes from "./routes/Routes";
@@ -8,9 +10,23 @@ import Placeholder from "./views/Placeholder";
 const Layout = () => {
   const [color, setColor] = useState("#002f6c");
 
+  const r = parseInt(color.slice(1, 3), 16),
+    g = parseInt(color.slice(3, 5), 16),
+    b = parseInt(color.slice(5, 7), 16);
   return (
-    <div style={{ backgroundColor: color, transition: "background-color 1.5s ease" }}>
-      <div style={{ overflow: "auto", height: "100vh" }}>
+    <div
+      style={{
+        backgroundImage: `url(${background})`
+      }}
+    >
+      <div
+        style={{
+          overflow: "auto",
+          height: "100vh",
+          backgroundColor: `rgba(${r},${g},${b},0.4)`,
+          transition: "background-color 1.5s ease"
+        }}
+      >
         <div style={{ position: "absolute", right: 15, zIndex: 1000 }}>
           <Navbar color={color} onColourChanged={color => setColor(color)} />
         </div>
