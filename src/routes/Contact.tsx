20b152b30acf0840a37f7ce28953983af7293547
Adam Young <adamyoungg97@gmail.com>
2020-09-25 14:17:39 +0100

    Reverted form changes

--- src/routes/Contact.tsx
+++ src/routes/Contact.tsx
@@ -1,66 +1,29 @@
-import Axios from 'axios';
-import React, { useState } from 'react';
-import { Alert, Button, Col, Form } from 'react-bootstrap';
+import React from 'react';
+import { Button, Col, Form } from 'react-bootstrap';
 
 import { DetailCard } from '../views/card/DetailCard';
 
 const Contact = () => {
-  const [name, setName] = useState('');
-  const [emailAddress, setEmailAddress] = useState('');
-  const [message, setMessage] = useState('');
-  const [submitted, setSubmitted] = useState(false);
-
-  const handleSubmit = async (e: React.FormEvent<HTMLElement>) => {
-    await Axios.post(
-      '/',
-      { 'form-name': 'contact', name, email: emailAddress, message },
-      { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
-    )
-      .then(() => setSubmitted(true))
-      .catch((error) => alert(error));
-
-    e.preventDefault();
-  };
-
   return (
     <DetailCard title='Contact' description="Pop your details below, and I'll get back to you as soon as possible.">
       <Col xs={12}>
-        <Form name='contact' onSubmit={handleSubmit}>
+        <Form name='contact' method='post'>
           <input type='hidden' name='form-name' value='contact' />
           <Form.Group controlId='name'>
             <Form.Label>Your Name</Form.Label>
-            <Form.Control
-              type='text'
-              placeholder='Name'
-              value={name}
-              onChange={(event) => setName(event.target.value)}
-            />
+            <Form.Control type='text' placeholder='Name' />
           </Form.Group>
 
           <Form.Group controlId='email'>
             <Form.Label>Your Email</Form.Label>
-            <Form.Control
-              type='email'
-              placeholder='Email address'
-              value={emailAddress}
-              onChange={(event) => setEmailAddress(event.target.value)}
-            />
+            <Form.Control type='email' placeholder='Email address' />
           </Form.Group>
 
           <Form.Group controlId='message'>
-            <Form.Control
-              as='textarea'
-              type='text'
-              placeholder='Your message'
-              value={message}
-              onChange={(event) => setMessage(event.target.value)}
-            />
+            <Form.Control as='textarea' type='text' placeholder='Your message' />
           </Form.Group>
 
-          <Button disabled={submitted} type='submit'>
-            Send
-          </Button>
-          {submitted && <Alert className='mt-4'>Submitted! Thanks for your message.</Alert>}
+          <Button type='submit'>Send</Button>
         </Form>
       </Col>
     </DetailCard>
