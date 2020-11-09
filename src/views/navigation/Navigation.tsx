683828c5fcc927c8921c2412a67bf1819c29f70c
Adam Young <adamyoungg97@gmail.com>
2020-11-09 10:44:32 +0000

    Added background splash

--- src/views/navigation/Navigation.tsx
+++ src/views/navigation/Navigation.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import styled from 'styled-components';
+
+const NavLink = styled(Nav.Link)`
+  color: white !important;
+  font-weight: bold;
+`;
 
 export const Navigation = () => {
   return (
-    <Navbar expand='lg' bg='transparent' fixed='top'>
+    <Navbar collapseOnSelect expand='lg' bg='transparent' variant='dark' fixed='top'>
       <Container>
         <Navbar.Brand>Adam Young</Navbar.Brand>
         <Navbar.Toggle aria-controls='navbar-nav' />
         <Navbar.Collapse id='navbar-nav'>
           <Nav className='ml-auto'>
-            <Nav.Link>Skills</Nav.Link>
-            <Nav.Link>Projects</Nav.Link>
-            <Nav.Link>Experience</Nav.Link>
-            <Nav.Link>Contact</Nav.Link>
-            <Nav.Link>Blog</Nav.Link>
+            <NavLink>Skills</NavLink>
+            <NavLink>Projects</NavLink>
+            <NavLink>Experience</NavLink>
+            <NavLink>Contact</NavLink>
+            <NavLink>Blog</NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
