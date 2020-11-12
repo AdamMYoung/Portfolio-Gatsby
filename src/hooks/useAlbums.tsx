c3eac71df81bf9179b9c368b521346a50434284d
Adam Young <adamyoungg97@gmail.com>
2020-11-12 17:49:40 +0000

    Updated refresh logic of hook

--- src/hooks/useAlbums.tsx
+++ src/hooks/useAlbums.tsx
@@ -4,5 +4,8 @@ import { getAlbums } from '../api/contentfulApi';
 export const useAlbums = () => {
   return useQuery('getAlbums', getAlbums, {
     cacheTime: 36000,
+    refetchOnWindowFocus: false,
+    refetchOnMount: false,
+    refetchOnReconnect: false,
   });
 };
