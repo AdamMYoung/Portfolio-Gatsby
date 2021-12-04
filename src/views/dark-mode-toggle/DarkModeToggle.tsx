7d6c15022bb0282f90348d251f4a212f32a95764
Adam Young <adamyoungg97@gmail.com>
2021-12-04 19:35:00 +0000

    fix to import ref

--- src/views/dark-mode-toggle/DarkModeToggle.tsx
+++ src/views/dark-mode-toggle/DarkModeToggle.tsx
@@ -1,5 +1,5 @@
 import { MoonIcon, SunIcon } from '@chakra-ui/icons';
-import { Button, useColorMode } from '@chakra-ui/react';
+import { Button, useBreakpointValue, useColorMode } from '@chakra-ui/react';
 import React from 'react';
 
 import { MenuIconButton } from '../../components';
