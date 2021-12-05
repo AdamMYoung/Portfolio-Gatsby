5d5b19435ab50ad7032a997288872d9285e0afd4
Adam Young <adamyoungg97@gmail.com>
2021-12-05 17:52:57 +0000

    Addded form support to Q&A

--- src/pages/q-and-a.tsx
+++ src/pages/q-and-a.tsx
@@ -1,5 +1,6 @@
 import { Button, FormControl, FormLabel, Input } from '@chakra-ui/react';
 import React, { VFC } from 'react';
+import { useForm } from 'react-hook-form';
 import {
     TwoPanel,
     TwoPanelBlock,
@@ -10,7 +11,12 @@ import {
 import { Layout } from '../views';
 import { SEO } from '../views/seo/SEO';
 
+const EMAIL_REGEX =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 const HeroIntro = () => {
+    const { register, handleSubmit } = useForm();
+
     return (
         <TwoPanel>
             <TwoPanelBlock>
@@ -21,29 +27,40 @@ const HeroIntro = () => {
             </TwoPanelBlock>
 
             <TwoPanelBlock>
-                <FormControl id="name">
-                    <FormLabel>Name</FormLabel>
-                    <Input borderRadius="full" placeholder="Jane Doe" />
-                </FormControl>
-                <FormControl id="emailAddress">
-                    <FormLabel>Email Address</FormLabel>
-                    <Input borderRadius="full" placeholder="janedoe@example.com" />
-                </FormControl>
-                <FormControl id="question">
-                    <FormLabel>Your Question</FormLabel>
-                    <Input
-                        as="textarea"
-                        borderRadius="3xl"
-                        minH="100px"
-                        py="3"
-                        sx={{
-                            '&::-webkit-scrollbar': { width: 0 },
-                            overflow: '-moz-scrollbars-none',
-                            '-ms-overflow-style': 'none',
-                        }}
-                    />
-                </FormControl>
-                <Button variant="outline">Submit</Button>
+                <form method="post" netlify-honeypot="bot-field" data-netlify="true" name="q-and-a">
+                    <input type="hidden" name="bot-field" />
+                    + <input type="hidden" name="form-name" value="contact" />
+                    <FormControl id="name">
+                        <FormLabel>Name</FormLabel>
+                        <Input borderRadius="full" placeholder="Jane Doe" {...register('name', { required: true })} />
+                    </FormControl>
+                    <FormControl id="emailAddress">
+                        <FormLabel>Email Address</FormLabel>
+                        <Input
+                            borderRadius="full"
+                            placeholder="janedoe@example.com"
+                            {...register('emailAddress', { required: true, pattern: EMAIL_REGEX })}
+                        />
+                    </FormControl>
+                    <FormControl id="question">
+                        <FormLabel>Your Question</FormLabel>
+                        <Input
+                            as="textarea"
+                            borderRadius="3xl"
+                            minH="100px"
+                            py="3"
+                            sx={{
+                                '&::-webkit-scrollbar': { width: 0 },
+                                overflow: '-moz-scrollbars-none',
+                                '-ms-overflow-style': 'none',
+                            }}
+                            {...register('question', { required: true })}
+                        />
+                    </FormControl>
+                    <Button type="submit" variant="outline">
+                        Submit
+                    </Button>
+                </form>
             </TwoPanelBlock>
         </TwoPanel>
     );
