c402cdb1a4bcaf9e8ba35447837e9a73841f2c68
Adam Young <adamyoungg97@gmail.com>
2021-12-06 04:11:16 +0000

    added sitemap generation

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
             },
         },
         'gatsby-plugin-react-helmet',
-        'gatsby-plugin-sitemap',
+        'gatsby-plugin-advanced-sitemap',
         'gatsby-plugin-sharp',
         'gatsby-transformer-sharp',
         {
