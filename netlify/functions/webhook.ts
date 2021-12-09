ba849b1691163b816a05a057e4f4c7165dfb5020
Adam Young <adamyoungg97@gmail.com>
2021-12-09 15:56:30 +0000

    Improved function payload handling

--- netlify/functions/webhook.ts
+++ netlify/functions/webhook.ts
@@ -9,11 +9,25 @@ type WebhookPayload = {
     updatedAt: string;
 };
 
-const handler: Handler = async (event, context) => {
-    const result = JSON.parse(event.body) as WebhookPayload;
+const isPayload = (payload: any): payload is WebhookPayload => {
+    return (
+        typeof payload.title === 'string' &&
+        typeof payload.slug === 'string' &&
+        Array.isArray(payload.topics) &&
+        typeof payload.createdAt === 'string' &&
+        typeof payload.updatedAt === 'string'
+    );
+};
+
+const handler: Handler = async (event) => {
+    const result = JSON.parse(event.body);
+
+    if (!isPayload(result)) {
+        return { statusCode: 400 };
+    }
 
     if (result.createdAt !== result.updatedAt) {
-        return { statusCode: 200 };
+        return { statusCode: 200, body: 'Article is not a new post, no action has been performed.' };
     }
 
     await axios.post(process.env.IFTTT_WEBHOOK_URL, {
@@ -22,7 +36,7 @@ const handler: Handler = async (event, context) => {
         value3: result.topics.reduce((prev, curr) => prev + `#${curr.replace('-', '')} `, ''),
     });
 
-    return { statusCode: 200 };
+    return { statusCode: 200, body: 'Article is a new post, social media posts have been submitted' };
 };
 
 export { handler };
