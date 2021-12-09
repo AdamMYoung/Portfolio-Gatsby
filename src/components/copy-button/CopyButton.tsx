a6b960a20ff097c2d4104dfadc2a0c3e9d109b36
Adam Young <adamyoungg97@gmail.com>
2021-12-09 17:03:00 +0000

    Added code copy

---
+++ src/components/copy-button/CopyButton.tsx
@@ -0,0 +1,24 @@
+import React, { FC, MouseEventHandler, useCallback } from 'react';
+import { Button, ButtonProps, useToast } from '@chakra-ui/react';
+
+type CopyButtonProps = ButtonProps & {
+    contentToCopy: string;
+};
+
+export const CopyButton: FC<CopyButtonProps> = ({ contentToCopy, onClick, ...rest }) => {
+    const toast = useToast();
+
+    const copyContentToClipboard = useCallback(() => {
+        navigator.clipboard.writeText(contentToCopy);
+
+        console.log(contentToCopy);
+        toast({ title: 'Copied!', description: 'Content copied to clipboard', status: 'success', duration: 3000 });
+    }, [contentToCopy]);
+
+    const handleClick: MouseEventHandler<HTMLButtonElement> = (e) => {
+        copyContentToClipboard();
+        onClick?.(e);
+    };
+
+    return <Button onClick={handleClick} fontSize="12" lineHeight="1" py="2" h="auto" {...rest} />;
+};
--- src/components/markdown-renderer/MarkdownRenderer.tsx
+++ src/components/markdown-renderer/MarkdownRenderer.tsx
@@ -2,9 +2,10 @@ import ChakraUIRenderer from 'chakra-ui-markdown-renderer';
 import React, { VFC } from 'react';
 import ReactMarkdown from 'react-markdown';
 import Highlight from 'react-highlight';
-import { Text, Box, Code, Heading } from '@chakra-ui/react';
+import { Text, Box, Code, Heading, Button } from '@chakra-ui/react';
 
 import { Link } from '~components';
+import { CopyButton } from '~components/copy-button/CopyButton';
 
 type MarkdownRendererProps = {
     markdown: string;
@@ -18,22 +19,29 @@ const newTheme = {
     h4: (props) => <Heading as="h4" pt="8" {...props} />,
     h5: (props) => <Heading as="h5" pt="8" {...props} />,
     h6: (props) => <Heading as="h6" pt="8" {...props} />,
-    a: ({ href, children }) => <Link href={href}>{children}</Link>,
+    a: (props) => <Link {...props} />,
     code: ({ children }) => (
         <Code p="1" wordBreak="break-word">
             {children}
         </Code>
     ),
-    pre: ({ children }) => {
+    pre: ({ node, children }) => {
+        //Odd prop drilling, since the code component is used for both single line code and pre.
+        const content = node.children[0].children[0].value;
+
         return (
             <Box
                 as="pre"
+                position="relative"
                 bg="gray.700"
                 boxShadow="xl"
                 rounded="xl"
                 pr="4"
                 sx={{ code: { w: 'full', color: 'white', bg: 'initial' } }}
             >
+                <CopyButton contentToCopy={content} position="absolute" top="4" right="4">
+                    Copy
+                </CopyButton>
                 <Highlight>{children}</Highlight>
             </Box>
         );
