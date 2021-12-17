3c5e629ee426281754aa45213560e0ecb33cb6d4
Adam Young <adamyoungg97@gmail.com>
2021-12-17 00:06:37 +0000

    Migrated to yarn pnp

--- src/hooks/use-is-mobile/useIsMobile.tsx
+++ src/hooks/use-is-mobile/useIsMobile.tsx
@@ -1,5 +1,9 @@
 import { useBreakpointValue } from '@chakra-ui/react';
 
+/**
+ * Hook to return true if the current breakpoint is mobile.
+ * @returns True if the current screen width is mobile size.
+ */
 export const useIsMobile = (): boolean => {
     return useBreakpointValue([true, null, false]);
 };
--- src/hooks/use-merged-styles/useMergedStyles.ts
+++ src/hooks/use-merged-styles/useMergedStyles.ts
@@ -2,6 +2,12 @@ import { SystemStyleObject } from '@chakra-ui/react';
 import { merge } from 'lodash';
 import { useMemo } from 'react';
 
+/**
+ * Hook to deeply merge styles from multiple sources.
+ * @param sx Style object passed into the component.
+ * @param styles Internal styles of the component.
+ * @returns A deeply merged style object.
+ */
 export const useMergedStyles = (sx: SystemStyleObject = {}, styles: SystemStyleObject): SystemStyleObject => {
     return useMemo(() => merge(styles, sx ?? {}), [styles, sx]);
 };
