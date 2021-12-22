6edceef07ad76ad9ae926533eb25dc82af54e13d
Adam Young <adamyoungg97@gmail.com>
2021-12-22 13:50:41 +0000

    Tweaks to hook variable naming

--- src/hooks/use-if-client/useIfClient.tsx
+++ src/hooks/use-if-client/useIfClient.tsx
@@ -3,6 +3,6 @@
  * Useful in SSG environments such as Gatsby, when accessing window or document elements.
  */
 export const useIfClient = <T extends any>(func: () => T, defaultValue?: T): T | undefined => {
-    const isSsr = typeof window !== 'undefined';
-    return isSsr ? func() : defaultValue;
+    const isClient = typeof window !== 'undefined';
+    return isClient ? func() : defaultValue;
 };
