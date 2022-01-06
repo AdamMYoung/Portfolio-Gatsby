60db0db564e254d45ae9d6986893a6715d3cab5e
Adam Young <adamyoungg97@gmail.com>
2022-01-06 20:15:54 +0000

    Fixes to cookie banner tracking

--- src/hooks/use-cookie-banner/useCookieBanner.tsx
+++ src/hooks/use-cookie-banner/useCookieBanner.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, Button, HStack, ToastId, useToast, Box, chakra, Stack } from '@chakra-ui/react';
 import { useEffect, useRef } from 'react';
 
@@ -21,19 +21,24 @@ const isCookieRequestSet = (): boolean => {
 const setCookieAcceptance = (accepted: boolean) => {
     document.cookie = createCookieEntry(gaCookie, accepted);
     document.cookie = createCookieEntry(hotjarCookie, accepted);
-    (window as any).gtag('event', 'page-view', {
-        page_location: window.location.href,
-    });
 };
 
 export const useCookieBanner = () => {
+    const [trackPageView, setTrackPageView] = useState(false);
     const toast = useToast();
     const toastId = useRef<ToastId>();
 
     const handleResponse = (accepted: boolean) => {
         setCookieAcceptance(accepted);
         toast.close(toastId.current);
+        setTrackPageView(true)
     };
+
+    useEffect(() => {
+        (window as any).gtag('event', 'page-view', {
+            page_location: window.location.href,
+        });
+    }, [trackPageView])
 
     useEffect(() => {
         useIfClient(() => {
