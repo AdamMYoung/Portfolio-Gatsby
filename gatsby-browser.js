f9f7cf3abffc1b62d4a077a2363ebed1f2dab3fe
Adam Young <adamyoungg97@gmail.com>
2022-01-11 18:21:36 +0000

    Removed location from gatsby-browser and ssr

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import { LazyMotion, domAnimation } from 'framer-motion';
 import { ChakraProvider } from '@chakra-ui/react';
-import { Location } from '@reach/router';
 
 import { Layout } from './src/views';
 import theme from './src/theme';
 
 export const wrapRootElement = ({ element }) => {
     return (
-        <Location>
-            <LazyMotion features={domAnimation}>
-                <ChakraProvider theme={theme}>
-                    <Layout>{element}</Layout>
-                </ChakraProvider>
-            </LazyMotion>
-        </Location>
+        <LazyMotion features={domAnimation}>
+            <ChakraProvider theme={theme}>
+                <Layout>{element}</Layout>
+            </ChakraProvider>
+        </LazyMotion>
     );
 };
