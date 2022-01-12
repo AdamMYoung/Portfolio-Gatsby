67a780a30b6b63bbcc75b98f003b069ccc6c6920
Adam Young <adamyoungg97@gmail.com>
2022-01-12 14:47:54 +0000

    Improved link sizing on links page

--- src/pages/links.tsx
+++ src/pages/links.tsx
@@ -1,7 +1,7 @@
 import { Container, Flex, Heading, Stack } from '@chakra-ui/react';
 import React from 'react';
 
-import { LinkButton } from '~components';
+import { CategoryList, CategoryListItem, LinkButton } from '~components';
 import { SEO } from '~views';
 
 const Links = () => {
@@ -14,24 +14,47 @@ const Links = () => {
             />
             <Stack>
                 <Heading fontWeight="semibold">My Links</Heading>
-                <Flex w="full" sx={{ '*': { mt: 4, mr: 4 } }} flexWrap="wrap">
-                    <LinkButton href="/">Website</LinkButton>
-                    <LinkButton href="https://drive.google.com/file/d/1Ws1_C6_3ryHWTol3ZSRxq3vFrrag5WfT/view?usp=sharing">
+                <CategoryList w="full" gap="2" flexWrap="wrap">
+                    <CategoryListItem as={LinkButton} href="/">
+                        Website
+                    </CategoryListItem>
+                    <CategoryListItem
+                        as={LinkButton}
+                        href="https://drive.google.com/file/d/1Ws1_C6_3ryHWTol3ZSRxq3vFrrag5WfT/view?usp=sharing"
+                    >
                         Resumé
-                    </LinkButton>
-                    <LinkButton href="https://github.com/adammyoung">GitHub</LinkButton>
-                    <LinkButton href="https://twitter.com/AdamMYoung_">Twitter</LinkButton>
-                    <LinkButton href="mailto:adam@aydev.uk">Email</LinkButton>
-                    <LinkButton href="/blog">Blog</LinkButton>
-                    <LinkButton href="/uses">Uses</LinkButton>
-                    <LinkButton href="/rss.xml">RSS</LinkButton>
-                    <LinkButton href="https://www.instagram.com/adammyoungphotography/">
+                    </CategoryListItem>
+                    <CategoryListItem as={LinkButton} href="https://github.com/adammyoung">
+                        GitHub
+                    </CategoryListItem>
+                    <CategoryListItem as={LinkButton} href="https://twitter.com/AdamMYoung_">
+                        Twitter
+                    </CategoryListItem>
+                    <CategoryListItem as={LinkButton} href="mailto:adam@aydev.uk">
+                        Email
+                    </CategoryListItem>
+                    <CategoryListItem as={LinkButton} href="/blog">
+                        Blog
+                    </CategoryListItem>
+                    <CategoryListItem as={LinkButton} href="/uses">
+                        Uses
+                    </CategoryListItem>
+                    <CategoryListItem as={LinkButton} href="/rss.xml">
+                        RSS
+                    </CategoryListItem>
+                    <CategoryListItem as={LinkButton} href="https://www.instagram.com/adammyoungphotography/">
                         Photography Instagram
-                    </LinkButton>
-                    <LinkButton href="https://stackoverflow.com/users/5620846/adam-young">Stack Overflow</LinkButton>
-                    <LinkButton href="https://www.linkedin.com/in/adammichaelyoung/">LinkedIn</LinkButton>
-                    <LinkButton href="https://unsplash.com/@adammyoung">Unsplash</LinkButton>
-                </Flex>
+                    </CategoryListItem>
+                    <CategoryListItem as={LinkButton} href="https://stackoverflow.com/users/5620846/adam-young">
+                        Stack Overflow
+                    </CategoryListItem>
+                    <CategoryListItem as={LinkButton} href="https://www.linkedin.com/in/adammichaelyoung/">
+                        LinkedIn
+                    </CategoryListItem>
+                    <CategoryListItem as={LinkButton} href="https://unsplash.com/@adammyoung">
+                        Unsplash
+                    </CategoryListItem>
+                </CategoryList>
             </Stack>
         </Container>
     );
