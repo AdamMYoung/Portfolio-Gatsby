bf503830e02b8811ef08faa4966873cbd5153c0c
Adam Young <adamyoungg97@gmail.com>
2022-01-25 23:58:05 +0000

    Attempted fix to axios

--- netlify/functions/subscribe.ts
+++ netlify/functions/subscribe.ts
@@ -2,11 +2,6 @@ import { Handler } from '@netlify/functions';
 import axios from 'axios'
 import { md5 } from "hash-wasm"
 
-const mailchimpApi = axios.create({
-    baseURL: `https://${process.env.MAILCHIMP_SERVER}.api.mailchimp.com/3.0`,
-    auth: { username: 'anystring', password: process.env.MAILCHIMP_API_KEY },
-});
-
 const handler: Handler = async (event) => {
     const email = event.queryStringParameters.email;
 
@@ -16,11 +11,10 @@ const handler: Handler = async (event) => {
 
     const hash = await md5(email.toLowerCase());
 
-    const res = await mailchimpApi.put(
-        `/lists/${process.env.MAILCHIMP_LIST_ID}/members/${hash}`,
+    const res = await axios.put(`https://${process.env.MAILCHIMP_SERVER}.api.mailchimp.com/3.0/lists/${process.env.MAILCHIMP_LIST_ID}/members/${hash}`,
         { email_address: email, status_if_new: 'pending' },
-        { headers: { 'content-type': 'application/json' } }
-    )
+        { auth: { username: 'anystring', password: process.env.MAILCHIMP_API_KEY } })
+
 
     return { statusCode: 200, body: res.data }
 };
